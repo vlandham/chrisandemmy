feef704c952c037c24cf82d38607732788d71e35
Jim V <vlandham@gmail.com>
2013-04-10 20:35:44 -0500

    update to use straight js

---
+++ dash/compile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+coffee -o js/ -c coffee/vis.coffee
+
+# cat all_js/plugins.js all_js/script.js all_js/CustomTooltip.js all_js/libs/jquery-ui-1.8.21.custom.min.js all_js/selectToUISlider.jQuery.js all_js/libs/bootstrap-tooltip.js all_js/libs/crossfilter.min.js all_js/vis.js | uglifyjs -o js/vis.min.js
+
